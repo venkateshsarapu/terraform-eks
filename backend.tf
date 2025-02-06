449c2fc4467e27f17c3528cd16c9e4281f894969
Venkatesh <99860692+venkateshsarapu@users.noreply.github.com>
2025-02-07 00:02:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "venkys3" 
+    bucket         = "venkys3buc" 
     key            = "terraform/state.tfstate"    # Path inside the bucket
-    region         = "us-east-1"                  # Change to your region
+    region         = "us-west-1"                  # Change to your region
     encrypt        = true                         # Encrypt state file
     dynamodb_table = "terraform-state-lock"       # Table for state locking
   }
