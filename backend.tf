e35500e1b388edf781c7fc62b98c85827570b900
Venkatesh <99860692+venkateshsarapu@users.noreply.github.com>
2025-02-07 01:46:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "venkys3buc" 
+    bucket         = "venkys3buck" 
     key            = "terraform/state.tfstate"    # Path inside the bucket
     region         = "us-west-1"                  # Change to your region
    
